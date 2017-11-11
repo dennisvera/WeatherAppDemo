9811974d6bf0ce3753065bf63439e719b2bb3ccc
dennisvera <dennisvera.z@gmail.com>
2017-11-11 11:37:04 -0500

    .gitignore file created

---
+++ WeatherAppDemo/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  WeatherAppDemo
+//
+//  Created by Dennis Vera on 11/11/17.
+//  Copyright © 2017 Dennis Vera. All rights reserved.
+//
+
+import Foundation
+
+struct Constatnt {
+    let openWeatherAPIKey = "eed0c9c1fc5ed97518d97298e9f009eb"
+}
